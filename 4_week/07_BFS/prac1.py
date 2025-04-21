2061445595a545a3f55d00f82a8a69e6d4b7b336
gabmin <rlarkqals@gmail.com>
2025-04-21 17:28:17 +0900

    알고리즘 7주차 7번 문제 복습

--- 4_week/07_BFS/prac1.py
+++ 4_week/07_BFS/prac1.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-
 current_r, current_c, current_d = 7, 4, 0
 current_room_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -16,54 +15,54 @@ current_room_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-# 북 동 남 서
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
+def turn_left_direction(d):
+    new_d = (d + 3) % 4
 
+    return new_d
 
-# 방향 전환
-def get_d_index_when_rotate_to_left(d):
-    return (d + 3) % 4
+def turn_back_direction(d):
+    new_d = (d + 2) % 4
 
-
-# 후진
-def get_d_index_when_go_back(d):
-    return (d + 2) % 4
-
+    return new_d
 
 def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
     n = len(room_map)
     m = len(room_map[0])
-    count_of_departments_cleaned = 1  # 청소하는 칸의 개수
+
+    total_count = 1
     room_map[r][c] = 2
+
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]
+
     queue = deque([[r, c, d]])
 
-    # 큐가 비어지면 종료
     while queue:
         r, c, d = queue.popleft()
         temp_d = d
 
-        for i in range(4):
-            temp_d = get_d_index_when_rotate_to_left(temp_d)
-            new_r, new_c = r + dr[temp_d], c + dc[temp_d]
+        for p in range(4):
+            temp_d = turn_left_direction(temp_d)
+            new_r = r + dr[temp_d]
+            new_c = c + dc[temp_d]
 
-            # a
             if 0 <= new_r < n and 0 <= new_c < m and room_map[new_r][new_c] == 0:
-                count_of_departments_cleaned += 1
+                total_count += 1
                 room_map[new_r][new_c] = 2
                 queue.append([new_r, new_c, temp_d])
                 break
-
-            # c
-            elif i == 3:  # 갈 곳이 없었던 경우
-                new_r, new_c = r + dr[get_d_index_when_go_back(d)], c + dc[get_d_index_when_go_back(d)]
+            elif p == 3:
+                new_d = turn_back_direction(d)
+                new_r = r + dr[new_d]
+                new_c = c + dc[new_d]
                 queue.append([new_r, new_c, d])
 
-                # d
-                if room_map[new_r][new_c] == 1:  # 뒤가 벽인 경우
-                    return count_of_departments_cleaned
+                if room_map[new_r][new_c] == 1:
+                    return total_count
 
 
+
+# 57 가 출력되어야 합니다!
 print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
 current_room_map2 = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
