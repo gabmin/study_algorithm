181697ebcb20e9ec2e2f3e213fb62538aec8dd55
gabmin <rlarkqals@gmail.com>
2025-04-23 20:38:15 +0900

    알고리즘 5주차 7번 문제 수정

--- 5_week/07/index.py
+++ 5_week/07/index.py
@@ -8,13 +8,13 @@ def solution(board, moves):
 
                 if len(stack) != 0 and stack[-1] == board[j][i - 1]:
                     stack.pop()
-                    answer += 1
+                    answer += 2
                 else:
                     stack.append(board[j][i - 1])
 
                 board[j][i - 1] = 0
                 break
-    return answer * 2
+    return answer
 
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
