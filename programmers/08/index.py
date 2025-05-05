793c648e343bc2a6f567c672c062d8a99782923c
gabmin <rlarkqals@gmail.com>
2025-05-05 14:37:27 +0900

    알고리즘 프로그래머스 8번 문제 최종 수정

--- programmers/08/index.py
+++ programmers/08/index.py
@@ -2,44 +2,41 @@ from collections import deque
 
 board = ["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]
 
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
 def solution(board):
     n = len(board)
     m = len(board[0])
 
-    visited = []
+    visited = [[False] * m for _ in range(n)]
     start_point = []
+    direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
                 start_point = [i, j]
 
-    queue = deque([start_point])
-    answer = 0
+    queue = deque([[start_point[0], start_point[1], 0]])
+    visited[start_point[0]][start_point[1]] = True
 
     while queue:
-        r, c = queue.popleft()
-        visited.append([r, c])
-
-        print(r, c)
+        r, c, cnt = queue.popleft()
 
         if board[r][c] == 'G':
-            break
+            return cnt
 
-        for d in direction:
-            new_r = r + d[0]
-            new_c = c + d[1]
+        for dx, dy in direction:
+            new_r = r
+            new_c = c
 
-            while board[new_r][new_c] != 'D':
-                if n < new_r < 0 and m < new_c < 0:
-                    break
+            while 0 <= new_r + dx < n and 0 <= new_c + dy < m and board[new_r + dx][new_c + dy] != 'D':
+                new_r += dx
+                new_c += dy
 
-                if board[new_r][new_c] != 'D' and not [new_r, new_c] in visited:
-                    queue.append([r, c])
-                    answer += 1
+            if not visited[new_r][new_c]:
+                visited[new_r][new_c] = True
+                queue.append([new_r, new_c, cnt + 1])
 
-    return answer
+    return -1
 
 
 print(solution(board))
